8002bfdebbfd61415cd108ad49c86954f2efa8d7
mustafa-dev-nasr <mustafa.dev.nasr@gmail.com>
2024-09-13 00:51:36 +0300

    -Add Flavor-Specific Configurations for Development and Production Environments - Added `launch.json` in the `.vscode` -added tow main files for each flavor

--- lib/main.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:radary/core/routing/app_router.dart';
-import 'package:radary/radary_app.dart';
-
-void main() {
-  runApp( RadaryApp(appRouter: AppRouter(),));
-}
