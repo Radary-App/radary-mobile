a6481b57919b3c970ded239e92f671462756d01a
mustafa-dev-nasr <mustafa.dev.nasr@gmail.com>
2024-09-18 01:34:44 +0300

    Update App Icon and Onboarding Assets

--- lib/features/onboarding/data/repo/onboarding_items.dart
+++ lib/features/onboarding/data/repo/onboarding_items.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import '../../../../core/helpers/util/assets_data.dart';
 import '../../../../generated/l10n.dart';
 import '../models/onboarding_info.dart';
 
@@ -8,16 +9,16 @@ class OnboardingItems {
     return [
       OnboardingInfo(
           title: S.of(context).Accessibility,
-          description: S.of(context).AccessibilityDescription,
-          image: "assets/svgs/Onboarding_1.svg"),
+          description: S.of(context).Message1,
+          image: Assetsdata.welcome_1),
       OnboardingInfo(
-          title: S.of(context).Answer1,
+          title: S.of(context).Message2,
           description: S.of(context).Answer2,
-          image: "assets/svgs/Onboarding_2.svg"),
+          image: Assetsdata.welcome_3),
       OnboardingInfo(
-          title: S.of(context).Answer3,
+          title: S.of(context).Message3,
           description: S.of(context).Answer4,
-          image: "assets/svgs/Onboarding_3.svg"),
+          image: Assetsdata.welcome_3),
     ];
   }
 }
